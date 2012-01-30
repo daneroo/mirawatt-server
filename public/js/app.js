b66caf2c670aa09f1c651cad12c2f2f1d7f2fe08
Daniel Lauzon <daniel.lauzon@gmail.com>
2012-01-30 16:15:06 -0500

    add models for hour and day

--- public/js/app.js
+++ public/js/app.js
@@ -4,66 +4,13 @@ function hideURLBar(){
   MBP.hideUrlBar();
 }
 
-function randSensor(){
-    return Math.random()*.5+.5;
-}
-function randModel(x,numSensors){ // x is a Date
-    var row = [x];
-    for (s=0;s<numSensors;s++) row.push(randSensor());
-    return row;
-}
-
-function miraModel(x,numSensors){ // x is a Date
-    var row = [x];
-    // for (s=0;s<numSensors;s++) row.push(randSensor());
-    var sec = x.getSeconds()/60;
-    var m = (sec<=.25)?Math.abs(Math.sin(sec*8*Math.PI)):0;
-    var w = (sec>.25 && sec<=.5)?1-Math.abs(Math.sin(sec*8*Math.PI)):1;
-    m=4*m;w=4*w;
-    row.push(0-m-w,m,w);
-    for (s=3;s<numSensors;s++) row.push((sec>.5)?randSensor():0);
-    return row;
-}
-
-var sourceModel=randModel;// miraModel;
-
-function hueColorModel(numSensors,h){
-    var colors=[];
-    for (s=0;s<numSensors;s++) colors.push(hsl(h,1,.3+(s%3)/10));
-    for (s=0;s<numSensors;s++) colors.push(hsl(s/numSensors,1,.5));
-    return colors;
-}
-function rainbowColorModel(numSensors){
-    var colors=[];
-    for (s=0;s<numSensors;s++) colors.push(hsl(h,1,.3+(s%3)/10));
-    for (s=0;s<numSensors;s++) colors.push(hsl(s/numSensors,1,.5));
-    console.log('colors',colors);
-    return colors;
-}
-var colorModel=hueColorModel;
-
 var globalG
 function drawGraph(){
-    var numSamples=60;
-    var numSensors=10; // min 3 if
-    var data = [];
-    var t = new Date();
-    var i,s;
-
-    var labels=['Time'];
-    for (s=0;s<numSensors;s++) labels.push('sensor '+(s+1));
-
-    
-    for (i = numSamples; i >= 0; i--) {
-      var x = new Date(t.getTime() - i * 1000);
-      data.push(sourceModel(x,numSensors));
-    }
-
-
+    var model=app.currentModel;
     var options = {
-      title: 'Average Power over Time',
+      title: 'Power over Time',
       titleHeight: 24,
-      logscale : false,
+      // logscale : false,
 
       //showRoller: true, // allows controlling roller
       // rollPeriod: 3, // ok depends on scale
@@ -73,7 +20,7 @@ function drawGraph(){
       // highlightCircleSize: 10,
       strokeWidth: 2,
       axisLabelColor: 'gray',
-      colors:colorModel(numSensors,(x.getSeconds()%60)/60),
+      colors:model.colors,
       
       // axis:{
       //   'weight':{axisLabelWidth:20}
@@ -82,7 +29,7 @@ function drawGraph(){
       // drawXGrid: false,
       // drawYGrid: false,
       // axisLabelWidth:100, // doesn't seem to do anything
-      yAxisLabelWidth:25,
+      yAxisLabelWidth:15,
       xAxisLabelWidth:55, // default 50 dosn't quite cut it...
 
       showLabelsOnHighlight:false,
@@ -97,32 +44,26 @@ function drawGraph(){
       //showRoller: true,
       //valueRange: [0.0, 1.2],
       //labels: ['Time', 'Power (avg)']
-      labels: labels,
+      labels: model.labels,
       stackedGraph: true
     };
 
-    globalG = new Dygraph(document.getElementById("dygraph"), data,options);
-
-    setInterval(function() {
-      var x = new Date();  // current time      
-      // truncate - could be more efficient
-      var toremove=data.length-(numSamples-1);
-      if (toremove>0) data.splice(0,toremove);
-
-      data.push(sourceModel(x,numSensors));
-      var hue = (x.getSeconds()%60)/60;
-      var hue = (app.hue%60)/60;
-      globalG.updateOptions( { 'file': data, colors:colorModel(numSensors,hue) } );
-    }, 1000);
+    globalG = new Dygraph(document.getElementById("dygraph"), model.data,options);
 
 }
 
-var app = {
-  svc:null,
-  values:null,
-  hue:0
-}
+setInterval(function() {
+    app.currentModel.update(app.currentModel);
+    var opts =  $.extend({}, { 
+        file: app.currentModel.data ,
+        colors: app.currentModel.colors ,
+    },app.currentModel.options);
+    globalG.updateOptions( opts );
+}, 1000);
 
+var app = app || {};
+app.svc=null;
+app.currentModel = app.models[0];
 
 $(function(){
   hideURLBar();
@@ -143,7 +84,14 @@ $(function(){
   
   $('#dygraph').click(function(){
       console.log('change scope');
-      app.hue+=10;
+      for (i=0;i<app.models.length;i++){
+          if (app.models[i]===app.currentModel){
+              console.log('found model:',i);
+              app.currentModel=app.models[(i+1)%app.models.length];
+              break;
+          }
+      }
+      
   })
   //anchorZoomSetup();
   drawGraph();
