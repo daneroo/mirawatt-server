f914f95340c574f3803c765fea05f340eb6af6f8
Daniel Lauzon <daniel@cantor.imetrical.com>
2012-02-05 03:03:32 -0500

    added iMetrical request

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 // Dependancies for json-rpc and dnode clients 
 var dnode = require('dnode');
 var jsonrpc = require('./lib/jsonrpc-client');
+var request = require('request');
 
 // json-rpc part
 client = jsonrpc('http://localhost:3000/jsonrpc');
@@ -8,9 +9,9 @@ var method='zing';
 var params=[44]; //{ n: param }, 
 client.call(method,params,function(err, result) {
         if (err){
-            console.log('remote.zing('+params+') Error: ',err);
+            console.log('jsonrpc.zing('+params+') Error: ',err);
         } else {
-            console.log('remote.zing('+params+') = ' + result);
+            console.log('jsonrpc.zing('+params+') = ' + result);
         }
     }
 );
@@ -21,10 +22,21 @@ dnode.connect(7070, function (remote, conn) {
     var param=42;
     remote.zing(param, function (err,result) {
         if (err){
-            console.log('remote.zing('+param+') Error: ',err);            
+            console.log('dnode.zing('+param+') Error: ',err);            
         } else {
-            console.log('remote.zing('+param+') = ' + result);            
+            console.log('dnode.zing('+param+') = ' + result);            
         }
         conn.end();
     });
+});
+
+//iMetricalReq =  request.defaults("http://cantor.imetrical.com/iMetrical/feedsJSON.php");
+request.get({uri:"http://cantor.imetrical.com/iMetrical/feedsJSON.php", json : true},function(error,response,body){
+	console.log('---body--');
+	body.forEach(function(sc,i){
+		console.log('scope',sc.scopeId,sc.name);
+		sc.observations=sc.observations.slice(0,1);
+	});
+	console.log(JSON.stringify(body,null,2))
+	console.log('+++body--');
 });
